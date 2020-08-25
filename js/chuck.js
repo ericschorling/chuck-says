3f3f8e9c6a9f7abcf4b354cfb3dc7adf8f6ae1b2
Eric Schorling <eric.schorling@yahoo.com>
2020-08-25 14:19:16 -0400

    now with submit

--- js/chuck.js
+++ js/chuck.js
@@ -1,13 +1,14 @@
 "use strict"
 
 const url = `https://api.chucknorris.io/jokes/random?category=`
-
+const valueUrl = `https://api.chucknorris.io/jokes/categories`
 const clickMe = document.getElementById("refreshQuote")
 const quoteOut = document.getElementById('chuckSays')
 const chuckImg = document.getElementById('chuckImg')
 const formButton = document.querySelector("#submitForm")
 const formInput = document.querySelector("#categoryInput")
 const animalOutput = document.querySelector('#animalquote')
+//let category = 'dev'
 let defaultCategory = 'dev';
 
 const dataFromAPI=()=>{
@@ -28,10 +29,40 @@ clickMe.addEventListener('click', () => {
     chuckImg.src = response.icon_url })
 });
 
-formButton.addEventListener('click',e=>{
-    e.preventDefault();
+
+
+// formButton.addEventListener('click',e=>{
+//     e.preventDefault();
+    
+// });
+
+const displayDropdown = () => {
+    get(valueUrl).then (result=>{
+        const filterFunc =(cate)=>{
+            if (cate !== 'explicit' && cate !== 'animal'){
+                return cate
+            }
+        }
+        console.log(result)
+        const filteredArr = result.filter(filterFunc)
+        filteredArr.map(category=>{
+            const optionItem = document.createElement('option')           
+            formInput.appendChild(optionItem);
+            optionItem.id = category;
+            optionItem.innerHTML = category;
+            
+        })
+    })
+}
+const getChuckQuotes = document.getElementById('getChuckQuotes')
+
+getChuckQuotes.addEventListener('submit', e => {
+    e.preventDefault()
     get(url + formInput.value).then(result =>{
         quoteOut.innerHTML = result.value
     })
     console.log(getQuote('dev'))
-});
+    console.log('heyo')
+})
+
+displayDropdown()
